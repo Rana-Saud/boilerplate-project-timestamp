eda266e748cfd171f61483debec4358886679b08
Rana Saud <126066239+Rana-Saud@users.noreply.github.com>
2024-12-27 06:08:41 -0800

    initialize Timestamp Microservice boilerplate with basic setup and configuration

---
+++ index.js
@@ -0,0 +1,49 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.get("/api/:date?", function (req, res) {
+  const dateParam = req.params.date;
+
+  if (dateParam == null || dateParam == undefined || dateParam.trim() == "") {
+    const date = new Date()
+    return res.json({ unix: date.getTime(), utc: date.toGMTString() })
+  }
+
+  const timestamp = parseInt(dateParam, 10);
+  const datetime = !isNaN(timestamp) && dateParam.length === 13 ? new Date(timestamp) : new Date(dateParam);
+
+  if (isNaN(datetime.getTime())) {
+    return res.status(400).json({ error: "Invalid Date" });
+  }
+
+  return res.json({ unix: datetime.getTime(), utc: datetime.toGMTString() })
+})
+
+
+// Listen on port set in environment variable or default to 3000
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
